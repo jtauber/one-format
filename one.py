31eab5768883d8ecae03df337490b2a7acc0e313
James Tauber <jtauber@jtauber.com>
2025-08-18 22:12:40 -0400

    switch to namedtuples

--- one.py
+++ one.py
@@ -1,5 +1,4 @@
-from collections import defaultdict
-
+from collections import defaultdict, namedtuple
 
 class One:
     def __init__(self, file_path):
@@ -9,14 +8,15 @@ class One:
     def load_data(self):
         lines = open(self.file_path).readlines()
         self.header = lines[0].rstrip("\n").split("\t")
+        Row = namedtuple("Row", self.header)
         self.data = []
         self.index = defaultdict(dict)
         for row_num, line in enumerate(lines[1:]):
-            row = dict(zip(self.header, line.rstrip("\n").split("\t")))
+            row = Row(*line.rstrip("\n").split("\t"))
             self.data.append(row)
-            for col_name in self.header:
+            for col_num, col_name in enumerate(self.header):
                 if col_name.endswith("_id"):
-                    self.index[col_name][row[col_name]] = row_num
+                    self.index[col_name][row[col_num]] = row_num
 
     def get(self, **kwargs):
         assert len(kwargs) == 1  # for now
